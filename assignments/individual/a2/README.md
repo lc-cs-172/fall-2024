5eb9167294a3617cba6055448e7a401c7fbe2c2d
Alain Kägi <16820273+alainkaegi@users.noreply.github.com>
2024-09-25 06:50:55 -0700

    feat: Add the linear algebra individual assignment

---
+++ assignments/individual/a2/README.md
@@ -0,0 +1,41 @@
+# Assignment #2: Linear Algebra Functions
+
+This assignment is meant to give you some practice writing methods that deal
+with arrays/lists.
+
+Start Visual Studio Code, create a new folder called `linear` (`File` >
+`Open Project..` > `New Folder` > `linear` > `Open`), download the following
+files:
+
+* [`linear_algebra.py`](./linear_algebra.py), and
+* [`linear_algebra_test.py`](./linear_algebra_test.py),
+
+place these files in the folder just created, open `linear_algebra_text.py`, a
+beaker icon should appear on the left, click on that icon, click on `Configure
+Python Test`, select the `pytest framework`, select the `Root directory`, wait a
+few seconds, then the folder name (`linear`) should appear on the `TESTING` pane
+towards the left.  When you hover over the folder name, various icons will
+appear to its right.  One of these icons is the play button.  If you click on
+it, all tests defined in `linear_algebra_test.py` will run.
+
+You should now be able to run the tests.  12 should fail and 9 should pass
+(because those 9 are just verifying that your functions don't do certain
+things).
+
+Work your way through `linear_algebra.py`, completing each function so that it
+passes the associated tests.
+
+## Hints
+
+Don't modify `linear_algebra_test.py`.
+
+Each function has a `return` statement put there just so that the code compiles.
+Replace that statement with your own code (which will include some kind of
+`return` statement).
+
+Looking at the tests might give you a hint as to what the code is supposed to
+do.
+
+## What To Hand In
+
+Upload your version of `linear_algebra.py`.
---
+++ assignments/individual/a2/linear_algebra.py
@@ -0,0 +1,84 @@
+"""A linear algebra module.
+
+This is a collection of functions dealing with vectors and matrices.
+"""
+
+__author__ = 'Put your name here'
+
+import math
+
+def vmagnitude(v):
+    """Returns the magnitude of the vector v (which may be of any
+    length).
+
+    This is found by adding up the squares of all of the elements of v
+    and taking the square root of the total.
+    """
+
+    return -1.0
+
+def vsum(v, w):
+    """Returns the sum of vectors v and w.
+
+    This is a vector of the same length, each of whose elements is the
+    sum of the corresponding elements in v and w.
+    """
+
+    return None
+
+def vdifference(v, w):
+    """Returns the difference between vectors v and w.
+
+    This is a vector of the same length, each of whose elements is the
+    difference between the corresponding elements in v and w.
+    """
+
+    return None
+
+
+def velementwise_product(v, w):
+    """Returns the element-wise between vectors v and w.
+
+    This is a vector of the same length, each of whose elements is the
+    product of the corresponding elements in v and w.
+    """
+
+    return None
+
+def vdot_product(v, w):
+    """Returns the dot product of vectors v and w.
+
+    This is the sum of the products of the corresponding elements.
+    """
+
+    return -1.0
+
+def mdimensions(m):
+    """Returns, as an array of two elements, the dimensions of matrix m."""
+
+    return None
+
+def msum(m, n):
+    """Returns the element-wise sum of matrices m and n."""
+
+    return None
+
+def melementwise_product(m, n):
+    """Returns the element-wise product of matrices m and n."""
+
+    return None
+
+def mtranspose(m):
+    """Returns the transpose of m, that is, a matrix where element i, j
+    is element j, i from m.
+    """
+
+    return None
+
+def mproduct(m, n):
+    """Returns the matrix product of m and n.
+
+    (Search the web for a definition.)
+    """
+
+    return None
